cbe5f0030945fa8d0d7cf13c5d06b520323026ad
RubyBe <sidbenavente@hotmail.com>
2016-07-22 10:08:16 -0700

    add a test for spec #1 (accept a word and return it) which fails due to absence of class constructor and get method

---
+++ Objects/RepeatCounter.cs
@@ -0,0 +1,9 @@
+
+namespace WordCounter
+{
+  // a class that accepts a word and a string of words, counts the number of occurrences of the initial word within the string of words, and returns the count.
+  public class RepeatCounter
+  {
+
+  }
+}
---
+++ Tests/RepeatCounterTest.cs
@@ -0,0 +1,24 @@
+using Xunit;
+
+
+// Unit tests for the RepeatCounter class
+namespace WordCounter
+{
+  public class RepeatCounterTest
+  {
+    // a test of the ability for the program to accept a single-word string as an input and return it for display
+    [Fact]
+    public void RepeatCounter_CreatesARepeatCounterObjectWithAWordProperty_ReturnsValueOfWordProperty()
+    {
+      // Arrange
+      string testString = "antelope";
+
+      // Act
+      RepeatCounter testRepeatCounter = new RepeatCounter(testString);
+      string resultString = testRepeatCounter.GetWord();
+
+      // Assert
+      Assert.Equal(testString, resultString);
+    }
+  }
+}
