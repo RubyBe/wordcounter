910d186f107feb419bcab277645aa8673cac27b8
RubyBe <sidbenavente@hotmail.com>
2016-07-22 11:41:10 -0700

    update RepeatCounter constructor to accept two strings; refactor spec1 and spec2 tests to accomodate new constructor

--- Objects/RepeatCounter.cs
+++ Objects/RepeatCounter.cs
@@ -7,20 +7,26 @@ namespace WordCounter
     // -------------------------------------------------------------------------
     // properties
     private string _word;
+    private string _stringOfWords;
 
     // -------------------------------------------------------------------------
     // constructors, getters, and setters
 
-    // a constructor which accepts a single string parameter
-    public RepeatCounter(string word)
+    // a constructor which requires 2 parameters: a string which will hold a single word, and a string which will hold a string of words
+    public RepeatCounter(string word, string stringofwords)
     {
       _word = word;
+      _stringOfWords = stringofwords;
     }
-
     // a getter which returns the value of the _word property
     public string GetWord()
     {
       return _word;
+    }
+    // a getter which returns the value of the _stringOfWords property
+    public string GetStringOfWords()
+    {
+      return _stringOfWords;
     }
 
     // -------------------------------------------------------------------------
--- Tests/RepeatCounterTest.cs
+++ Tests/RepeatCounterTest.cs
@@ -8,30 +8,23 @@ namespace WordCounter
 {
   public class RepeatCounterTest
   {
-    // a test of the ability for the program to accept a single-word string as an input and return it for display
+    // Spec1: test of the ability for the program to accept a single-word string as an input and return it for display - test removed and combined with Spec2 test
+
+    // Spec2: test of the ability of the program to accept a string of words as an input and return it for display - combining tests for spec 1 and 2 to accomodate a single constructor which accepts two strings
     [Fact]
-    public void RepeatCounter_CreatesARepeatCounterObjectWithAWordProperty_ReturnsValueOfWordProperty()
+    public void RepeatCounter_CreatesARepeatCounterObjectWithTwoStrings_ReturnsValueOfTwoStrings()
     {
       // Arrange
       string testWord = "antelope";
-      // Act
-      RepeatCounter testRepeatCounter = new RepeatCounter(testWord);
-      string resultString = testRepeatCounter.GetWord();
-      // Assert
-      Console.WriteLine("Spec 1 expected: " + testWord + " and actual: " + resultString);
-      Assert.Equal(testWord, resultString);
-    }
-
-    // a test of the ability of the program to accept a string of words as an input and return it for display
-    [Fact]
-    public void RepeatCounter_CreatesARepeatCounterObjectWithAStringOfWordsProperty_ReturnsValueOfStringProperty()
-    {
-      // Arrange
       string testString = "An antelope is like a gazelle in a way but it begins with an A actually.";
       // Act
-      RepeatCounter testRepeatCounter = new RepeatCounter(testString);
-      string resultString = testRepeatCounter.GetString();
+      RepeatCounter testRepeatCounter = new RepeatCounter(testWord, testString);
+      string resultWord = testRepeatCounter.GetWord();
+      string resultString = testRepeatCounter.GetStringOfWords();
       // Assert
+      Console.WriteLine("Spec 1 expected: " + testWord + " Spec1 actual: " + resultWord);
+      Console.WriteLine("Spec 2 expected: " + testString + " Spect2 actual: " + resultString);
+      Assert.Equal(testWord, resultWord);
       Assert.Equal(testString, resultString);
     }
   }
